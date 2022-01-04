5b536d8d0f9104cbe9d724a0b85e8dfe3fcaf21f
yoshihiro <yoshi.maruyama02@gmail.com>
2022-01-04 21:18:34 +0900

    Integrated friendly routes for blog posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -56,7 +56,8 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      # friendlyID Gem
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
