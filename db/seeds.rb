b91f15a80db40f1c95bef6986cbfb11ad2d2f964
yoshihiro <yoshi.maruyama02@gmail.com>
2022-01-05 14:09:16 +0900

    implemented custom scope for portfolio item

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
+3.times do |i|
+  Topic.create(title: "Topic #{i}")
+end
+
 10.times do |i|
   Blog.create!(
     title: "My blog post #{i}",
-    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,14 +21,22 @@ end
 
 puts "5 skills created"
 
-9.times do |i|
+8.times do |i|
   Portfolio.create!(
     title: "Porfolio title #{i}",
-    subtitle: "My greate service",
+    subtitle: "Ruby on Rails",
     body: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, ",
     main_image: "https://placehold.jp/600x400.png",
     thumb_image: "https://placehold.jp/350x250.png",
   )
 end
+
+Portfolio.create!(
+  title: 'Portfolio title 10',
+  subtitle: "Angular",
+  body: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, ",
+  main_image: "https://placehold.jp/600x400.png",
+  thumb_image: "https://placehold.jp/350x250.png",
+)
 
 puts "9 portfolios created"
