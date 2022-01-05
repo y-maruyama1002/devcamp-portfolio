6c942533821c2b7f8e075983ae7fea2cace013c3
yoshihiro <yoshi.maruyama02@gmail.com>
2022-01-05 15:02:54 +0900

    implemented concern form manging image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -8,7 +9,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "https://placehold.jp/600x400.png"
-    self.thumb_image ||= "https://placehold.jp/350x250.png"
+    self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
+    self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")
   end
 end
