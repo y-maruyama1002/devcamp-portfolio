d037a313031b36e4d7365434f7e79fa2d6c8d3da
yoshihiro <yoshi.maruyama02@gmail.com>
2022-01-09 22:04:30 +0900

    Updated portfolio to include attr for positoin

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,6 +8,11 @@ class Portfolio < ApplicationRecord
   def self.angular
     where(subtitle: "Angular")
   end
+
+  def self.by_position
+    order("position ASC")
+  end
+
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
   after_initialize :set_defaults
 
