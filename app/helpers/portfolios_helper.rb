5a6a0b88f94be3159ece065793efb73d83690a7a
yoshihiro <yoshi.maruyama02@gmail.com>
2022-01-10 16:50:30 +0900

    Updated file uploadd forms and refactor functionality into view

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,15 @@
 module PortfoliosHelper
+  def image_generator(height:, width:)
+    "https://placehold.jp/#{height}x#{width}.png"
+  end
+
+  def portfolio_img(img, type)
+    if img
+      img
+    elsif type == "thumb"
+      image_generator(height: "350", width: "200")
+    else
+      image_generator(height: "600", width: "400")
+    end
+  end
 end
