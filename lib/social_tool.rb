c13167a0532171a02b467fc41df057e1a13175a1
yoshihiro <yoshi.maruyama02@gmail.com>
2022-01-11 01:06:41 +0900

    initial connect to Twitter API

---
+++ lib/social_tool.rb
@@ -0,0 +1,14 @@
+module SocialTool
+  def self.twitter
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
+      config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
+    end
+
+    client.serch("#rails", result_types: 'recent').take(6).collect do |tweet|
+      "#{tweet.user.screen_name}: #{tweet.text}"
+    end
+  end
+end
