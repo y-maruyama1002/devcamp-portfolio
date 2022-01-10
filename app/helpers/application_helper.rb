e7873e3a03167bac4137a5f2e5809d15f61fd96f
yoshihiro <yoshi.maruyama02@gmail.com>
2022-01-11 00:04:13 +0900

    refactored alert into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,7 +40,7 @@ module ApplicationHelper
       },
       {
         url: portfolios_path,
-        title: "Poartfolio"
+        title: "Portfolio"
       }
     ]
   end
@@ -55,5 +55,16 @@ module ApplicationHelper
 
   def active?(path)
     "active" if current_page?(path)
+  end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+      alert_generator(alert)
+    end
+  end
+
+  def alert_generator(msg)
+    js add_gritter(msg, title: "yoshi portfolio alert", :sticky => false, time: 3000)
   end
 end
