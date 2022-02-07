c95ebc192ceb72558f72b0ab7e42332036310eb4
yoshihiro <yoshi.maruyama02@gmail.com>
2022-02-08 08:25:26 +0900

    Implemented th blog widget for topics

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy toggle_status ]
+  before_action :set_sidebar_topics, except: [:create, :update, :destroy, :toggle_status]
   layout "blog"
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
@@ -85,5 +86,9 @@ class BlogsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def blog_params
       params.require(:blog).permit(:title, :body, :topic_id)
+    end
+
+    def set_sidebar_topics
+      @set_sidebar_topics = Topic.with_blogs
     end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  before_action :set_sidebar_topics
   layout 'blog'
   def index
     @topics = Topic.all.page(params[:page]).per(5)
@@ -11,5 +12,11 @@ class TopicsController < ApplicationController
     else
       @blogs = @topic.blogs.published.recent.page(params[:page]).per(5)
     end
+  end
+
+  private
+
+  def set_sidebar_topics
+    @set_sidebar_topics = Topic.with_blogs
   end
 end
